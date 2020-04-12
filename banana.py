e0133d1d8317c30ff836bfb8c4e86c0cbe09d52f
manuhana <rinut.manuhana@gmail.com>
2020-04-12 22:19:09 +0700

    version 0.1.0

---
+++ banana.py
@@ -0,0 +1,12 @@
+border = '-' * 32
+print('-'*15 + '\/' + '-'*15)
+print('| '+ ' Welcome to the banana world ' + '|')
+print('-'*15 + '/\\' + '-'*15)
+print('Hi, my name is Banana.\nI\'m not male nor female.\nLet\'s just say, I\'m Banana.\nOkay? :)')
+print(border)
+print('May I know your name?')
+name = input()
+MC_name = name
+print(border)
+print('So, you\'re name is ' + MC_name + ', right?')
+print('[1] Yes\n[2] No\nNote: Your may choose the answer \nby input the number on your screen')
